4038d40ee756a85c0d67c568c6325cf6abe5a206
PMK <355176+PMK@users.noreply.github.com>
2026-04-18 23:15:45 +0200

    feat: add backend-only

---
+++ dist/index.backend-only.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2026-present Pixelrunner (https://pixelrunner.dev)
+ * Distributed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
+ * International license (CC BY-NC-ND 4.0). To view a copy of this license, visit
+ * https://creativecommons.org/licenses/by-nc-nd/4.0/.
+ *
+ * @copyright Pixelrunner (https://pixelrunner.dev)
+ * @license CC-BY-NC-ND-4.0
+ */
+export { ChildManager, cm } from './lib/ChildManager.js';
+export { logger } from './lib/Logger.js';
+//# sourceMappingURL=index.backend-only.d.ts.map
