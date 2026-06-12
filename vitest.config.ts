2eedecbf348b15ee5cd8263eb0211844fdb01834
PMK <355176+PMK@users.noreply.github.com>
2026-06-12 23:30:24 +0200

    test: add github action reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     environment: 'node',
     dir: './test/',
+    reporters: process.env.GITHUB_ACTIONS ? ['dot', 'github-actions'] : ['default'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html']
